9dd7f72e78639d584f142830dbd6c5e88ba5dc58
George Liu <eva2000@centminmod.com>
2019-03-05 20:30:22 +1000

    add curltest

---
+++ curltest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#######################################
+# curltest br url
+# curltest gzip url
+#######################################
+set -e
+
+bytesToHuman() {
+    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,E,P,Y,Z}iB)
+    while ((b > 1024)); do
+        d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+        b=$((b / 1024))
+        let s++
+    done
+    echo "$b$d ${S[$s]}"
+}
+
+compare() {
+  echo "URI: ${2} (${1})"
+
+  SIZE=$(curl -kso /dev/null "${2}" -w '%{size_download}')
+  SIZE_HUMAN=$(bytesToHuman "$SIZE")
+  echo "Uncompressed size : $SIZE_HUMAN"
+
+  SIZE=$(curl -H "Accept-Encoding: ${1}" -kso /dev/null "${2}" -w '%{size_download}')
+
+  SIZE_HUMAN=$(bytesToHuman "$SIZE")
+  echo "Compressed size   : $SIZE_HUMAN"
+}
+
+compare $1 $2
