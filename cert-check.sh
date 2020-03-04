25160401521baccc79e009501e62beeaed448e43
George Liu <eva2000@centminmod.com>
2020-03-05 09:57:12 +1000

    update cert-check.sh
    add check for HTTP/3 unsupported domains

--- cert-check.sh
+++ cert-check.sh
@@ -3,9 +3,19 @@ export LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu'
 
 certcheck() {
   domain=$1
-  TLSPROTOCOL_H3=$(/usr/local/src/curl/src/curl --http3 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
-  TLSPROTOCOL_H2=$(/usr/local/src/curl/src/curl --http2 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
-  TLSPROTOCOL_H1=$(/usr/local/src/curl/src/curl --http1.1 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
+  TLSPROTOCOL_H3=$(/usr/local/src/curl/src/curl --connect-timeout 1 --http3 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
+  TLSPROTOCOL_H2=$(/usr/local/src/curl/src/curl --connect-timeout 1 --http2 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
+  TLSPROTOCOL_H1=$(/usr/local/src/curl/src/curl --connect-timeout 1 --http1.1 -4Iv https://$domain 2>&1 | awk '/^HTTP/ {print $1}')
+
+  if [[ -z "$TLSPROTOCOL_H3" ]]; then
+    TLSPROTOCOL_H3=NA
+  fi
+  if [[ -z "$TLSPROTOCOL_H2" ]]; then
+    TLSPROTOCOL_H2=NA
+  fi
+  if [[ -z "$TLSPROTOCOL_H1" ]]; then
+    TLSPROTOCOL_H1=NA
+  fi
 
   certinfo -domain $domain | jq -r '"\(.subject.common_name)|\(.serial_number)|\(.issuer.organization)|\(.not_after)|\(.sans)"' | awk -v v3=$TLSPROTOCOL_H3 -v v2=$TLSPROTOCOL_H2 -v v1=$TLSPROTOCOL_H1 '{print $0"|"v3"|"v2"|"v1}'
 }
